02538a060f233cf3f5a2a1858548236a74b19909
Peter Banka <peter.banka@cyaninc.com>
2014-11-05 16:44:43 -0800

    Finished with example

--- src/talk3/SearchBar.js
+++ src/talk3/SearchBar.js
@@ -3,12 +3,31 @@
 var React = require('react');
 
 var SearchBar = React.createClass({
+    handleChange: function() {
+        this.props.onUserInput(
+            this.refs.filterTextInput.getDOMNode().value,
+            this.refs.inStockOnlyInput.getDOMNode().checked
+        );
+    },
     render: function() {
         return (
             <form>
-                <input type="text" placeholder="Search..." value={this.props.filterText} />
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    value={this.props.filterText}
+                    ref="filterTextInput"
+                    onChange={this.handleChange}
+                    id="search-box"
+                />
                 <p>
-                    <input type="checkbox" value={this.props.inStockOnly} />
+                    <input
+                        type="checkbox"
+                        value={this.props.inStockOnly}
+                        ref="inStockOnlyInput"
+                        onChange={this.handleChange}
+                        id="check-box"
+                    />
                     Only show products in stock
                 </p>
             </form>
--- src/talk3/__tests__/FilterableProductTable-test.js
+++ src/talk3/__tests__/FilterableProductTable-test.js
@@ -2,29 +2,58 @@
 
 jest.dontMock('../ProductTable.js');
 jest.dontMock('../FilterableProductTable.js');
+jest.dontMock('../SearchBar.js');
 var React = require('react/addons');
 var TestUtils = React.addons.TestUtils;
 var FilterableProductTable = require('../FilterableProductTable.js');
+var SearchBar = require('../SearchBar.js');
 
-var PRODUCTS = [{
-    category: 'thing',
-    name: 'glove',
-    price: '$0.50',
-    stocked: true,
-}];
+var PRODUCTS = [
+    {category: 'thing', name: 'glove', price: '$0.50', stocked: true},
+    {category: 'thing', name: 'spam', price: '$1.50', stocked: true},
+    {category: 'thing', name: 'glam', price: '$9.50', stocked: false},
+    {category: 'thing', name: 'blam', price: '$99.00', stocked: true},
+    {category: 'thing', name: 'sham', price: '$0.20', stocked: true},
+];
 
 describe('FilterableProductTable', function() {
     it('creates the entire table', function () {
-        var filterText = '';
-        var inStockOnly = false;
         filterableProductTable = TestUtils.renderIntoDocument(
             <FilterableProductTable
                 products={PRODUCTS}
-                filterText = {filterText}
-                inStockOnly = {inStockOnly}
+                filterText = {''}
+                inStockOnly = {false}
              />
         );
         var rows = TestUtils.scryRenderedDOMComponentsWithTag(filterableProductTable, 'tr');
-        expect(rows.length).toEqual(3);
+        expect(rows.length).toEqual(7);
+    });
+
+    it('searches the table for proper stuff', function() {
+        filterableProductTable = TestUtils.renderIntoDocument(
+            <FilterableProductTable
+                products={PRODUCTS}
+                filterText = {''}
+                inStockOnly = {false}
+             />
+        );
+        /*
+        // var inputDomElement = document.querySelectorAll('#search-box')[0];
+        // console.log('>>>>>>>>>>>>>' + inputDomElement.innerHTML);
+        var inputObjects = TestUtils.scryRenderedDOMComponentsWithTag(filterableProductTable, 'input');
+        var inputDomElement = inputObjects[0];
+        console.log('>>>>>>>>>>>>> ' + inputDomElement.textContent);
+        var inputBox = inputObjects[0];
+        // TestUtils.Simulate.keyUp(inputBox, {key: 'a'});
+        TestUtils.Simulate.change(inputBox, {target: {value: 'a'}});
+        console.log('>>>>>>>>>>>>> ' + inputDomElement.textContent);
+        inputDomElement.textContent);
+        var rows = TestUtils.scryRenderedDOMComponentsWithTag(filterableProductTable, 'tr');
+        expect(rows.length).toEqual(6);
+        */
+        var el = document.querySelectorAll('#search-box')[0];
+        //var event = document.createEvent('HTMLEvents');
+        var rows = TestUtils.scryRenderedDOMComponentsWithTag(filterableProductTable, 'tr');
+        // expect(rows.length).toEqual(6);
     });
 });
